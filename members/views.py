c4ad7a12d210eef4e5b648eb85caf63048884f56
Petur Ivanov <peturivanov4712@gmail.com>
2023-08-23 18:08:49 +0300

    Register form and style changes

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 def login_user(request):
@@ -29,7 +30,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -40,7 +41,7 @@ def register_user(request):
             return redirect('home')
 
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
     return render(request, 'authenticate/register_user.html', {'form': form})
 
