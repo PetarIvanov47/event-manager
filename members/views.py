c04c65e952b1166e8451cd6867e833361ce11d85
Petur Ivanov <peturivanov4712@gmail.com>
2023-08-23 15:09:48 +0300

    register user

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login_user(request):
@@ -24,6 +25,24 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You were logged out!')
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration Successful!')
+            return redirect('home')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {'form': form})
 
 
 
